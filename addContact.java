9432c569c7bb9aeaab13416c23eaffbd569e8aca
Hephy <40155584+MissKidda@users.noreply.github.com>
2018-12-17 14:57:39 -0600

    Create addContact.java

---
+++ addContact.java
@@ -0,0 +1,39 @@
+package phoneBook;
+
+
+
+public class addContact {
+	private static Person [] people;
+	
+	public void searchName (String fname) {
+		for (int i=0; i< people.length; i++) {
+			if(people[i].getFirstName().trim().toUpperCase().equals(fname.toUpperCase())) { 
+				System.out.println(people[i]);
+		}
+	}
+	
+	
+	}	
+	
+	public void removeRecord (String numDel) {
+
+		int j=0,temp=0;
+		for( int i = 0; i < people.length; i++) {
+			if(numDel.toUpperCase().trim().equals(people[i].getContact().trim().toUpperCase())){
+				temp =i;
+
+			}
+		}
+
+		for (int k = temp; k < people.length-1; k++) {
+
+			people[k] = people[k+1];
+			j++;
+
+		}
+		people[people.length-1] = null;
+	}
+	
+}
+	
+
