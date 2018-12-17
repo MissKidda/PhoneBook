bd64479686d302ae1a0d46f474c619be6097b115
Hephy <40155584+MissKidda@users.noreply.github.com>
2018-12-17 14:56:56 -0600

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,55 @@
+package phoneBook;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Address {
+
+	
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	
+	Address(){}
+	public Address(String street, String city, String state, String zip) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+
+	public String toString() {
+		return this.street+" "+ this.city+" "+ this.state+" "+ this.zip;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	}
+
+
+
